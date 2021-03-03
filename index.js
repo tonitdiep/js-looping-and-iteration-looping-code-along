7941d28962e7e0123564f0c316bb90e28efc6950
tonitdiep <adiep123@gmail.com>
2021-03-03 05:12:30 -0500

    commit tree f5c2996ed36441ba1b6340f1f0992fcedc0d1ea0

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solutions in this file
+function writeCards( arrayNames, event) {
+    let thankYouCards = []
+    for (let i = 0; i < arrayNames.length; i++ ) {
+        thankYouCards.push ( `Thank you, ${arrayNames[i]}, for the wonderful ${event} gift!` )
+    }
+    return thankYouCards
+}
+
+function countDown ( startNumber ) {
+    while ( startNumber > 0 ) {
+        console.log (startNumber )
+        startNumber -= 1
+    }
+    console.log ( startNumber )
+}
